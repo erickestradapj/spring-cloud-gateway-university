c688a8ce98711380164c4b225be7eff4683bb43f
Erick Estrada <erickestradapj@gmail.com>
2021-12-04 19:18:57 -0500

    properties added

--- src/main/java/com/dev/microservices/app/gateway/SpringCloudGatewayApplication.java
+++ src/main/java/com/dev/microservices/app/gateway/SpringCloudGatewayApplication.java
@@ -2,12 +2,14 @@ package com.dev.microservices.app.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringCloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
 
 }
